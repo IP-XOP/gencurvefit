ecd684038ca87a30b54b859785524e4f94fabf49
andyfaff <andyfaff@17ec3ff0-b92a-0410-b65e-9095fddf55e2>
2009-10-15 23:38:07 +0000

    cleaning up some code

---
+++ packages/gencurvefit/trunk/src/updateXOP.h
@@ -0,0 +1,13 @@
+
+
+/*
+ Functions contained in updateXOP<x>.c
+ */
+void DrawXOPWindow(XOP_WINDOW_REF w);
+void DisplayWindowXOP1Message(XOP_WINDOW_REF w, int numcoefs, double* coefs, double chi2,char* fitfunc,long fititers, float convergenceNumber);
+XOP_WINDOW_REF CreateXOPWindow(void);
+void DestroyXOPWindow(XOP_WINDOW_REF w);
+
+#ifdef _WINDOWS_			// [
+int CreateXOPWindowClass(void);
+#endif	
