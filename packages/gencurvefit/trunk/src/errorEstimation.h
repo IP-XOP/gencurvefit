82d4d47b2c53beaaf580a26f54ea01321024b1ec
andyfaff <andyfaff@17ec3ff0-b92a-0410-b65e-9095fddf55e2>
2009-10-15 23:36:48 +0000

    cleaning up some code

---
+++ packages/gencurvefit/trunk/src/errorEstimation.h
@@ -0,0 +1,14 @@
+#include "GenCurveFit.h"
+
+/*
+ functions contained in errorEstimation.c
+ */
+static int ludcmp(double**,int,int*,double*);
+static int lubksb(double **a, int n, int *indx, double b[]);
+static int matrixInversion(double **a, int N);
+int getCovarianceMatrix(GenCurveFitRuntimeParamsPtr p, GenCurveFitInternalsPtr goiP);
+int updatePartialDerivative(double**, GenCurveFitRuntimeParamsPtr p, GenCurveFitInternalsPtr goiP);
+int partialDerivative(double**, int, GenCurveFitRuntimeParamsPtr, GenCurveFitInternalsPtr,int);
+int updateAlpha(double**,double**, GenCurveFitInternalsPtr goiP);
+int calculateAlphaElement(int row, int col, double **alpha, double **derivativeMatrix, GenCurveFitInternalsPtr goiP);
+int packAlphaSymmetric(double** alpha,GenCurveFitInternalsPtr);
