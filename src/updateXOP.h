a8360fc1f6ad0b0879b1fe121383173d7780db18
andyfaff <andyfaff@17ec3ff0-b92a-0410-b65e-9095fddf55e2>
2010-09-29 06:53:49 +0000

    updated update function

--- src/updateXOP.h
+++ src/updateXOP.h
@@ -4,7 +4,7 @@
  Functions contained in updateXOP<x>.c
  */
 void DrawXOPWindow(XOP_WINDOW_REF w);
-void DisplayWindowXOP1Message(XOP_WINDOW_REF w, int numcoefs, double* coefs, double chi2,char* fitfunc,long fititers, double convergenceNumber);
+void DisplayWindowXOP1Message(XOP_WINDOW_REF w, int numcoefs, const double* coefs, double chi2,char* fitfunc,long fititers, unsigned int updatetime, double convergenceNumber);
 XOP_WINDOW_REF CreateXOPWindow(void);
 void DestroyXOPWindow(XOP_WINDOW_REF w);
 
